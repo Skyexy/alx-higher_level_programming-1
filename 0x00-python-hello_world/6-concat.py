39ee2c48e50b9998991d38a708ba643d0c2796c8
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-05-30 20:14:13 +0300

    My commit

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-<<<<<<< HEAD
-str += ' ' + str2
-=======
-str = str1 + ' ' + str2
->>>>>>> 193818ce26e89dfde0d3829bcf427398828a48dc
+str1 = str1 + ' ' + str2
 print("Welcome to {}!".format(str1))
