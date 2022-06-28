27b4bb3b4c0a6426c50fe93281717b757d69d7b6
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-06-28 19:40:02 +0300

    My commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,7 +6,7 @@ Module for a class that prevents dynamic attributes creation
 
 class LockedClass():
     """Class to prevent dynamic attributes creation"""
-    __slots__ = [first_name]
+    __slots__ = ['first_name']
 
     def __init__(self):
         """Init method"""
