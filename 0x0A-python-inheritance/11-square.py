b830eeab6661e514afd3f637ba5cc21725e22b26
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-07-04 21:39:28 +0300

    My commit

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Class that defines a Square from Rectangle class """
-Rectangle = __import__(9-rectangle).Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
