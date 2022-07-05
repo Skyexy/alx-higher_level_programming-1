6c54e4d6a33fb6a977ad30505efeaf5e86340c84
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-07-05 19:43:04 +0300

    My commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,6 +10,6 @@ def read_file(filename=""):
         Exception: when the file can be opened
     """
 
-    with open(filename, r, encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data, end=)
+        print(read_data, end='')
